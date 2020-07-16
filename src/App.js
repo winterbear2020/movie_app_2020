51b623d52bd19a25740f5e63c8bc8782a0138b2e
winterbear2020 <marswing@naver.com>
2020-07-17 05:20:08 +0900

    2020.07.17

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    count: 0,
-  };
-  render() {
-    return <h1>I'm a class component</h1>;
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 export default App;
---
+++ src/App_test2.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class App extends React.Component {
+  //state는 object component의 data를 넣을 공간 date that will be change
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
+export default App;
